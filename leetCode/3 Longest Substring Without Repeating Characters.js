a771a31e379704ec3cda0cd4c37aff778a6a813b
yang123guo <289656703@qq.com>
2017-06-16 23:25:49 +0800

    haode

--- leetCode/3 Longest Substring Without Repeating Characters.js
+++ leetCode/3 Longest Substring Without Repeating Characters.js
@@ -11,7 +11,7 @@ var lengthOfLongestSubstring = function(s) {
     var hash = {};
     var start = 0;
     var ans = 0;
-    
+
     for (var i = 0, len = s.length; i < len; i++) {
         var item = s[i];
 
@@ -19,8 +19,17 @@ var lengthOfLongestSubstring = function(s) {
           hash[item] = true;
         else {
           // item 已经在 substring 中存在了
-          for (; ;) {
-            if (s[start] === item) {
-              start++;
-              break;
+            for (; ;) {
+                if (s[start] === item) {
+                  start++;
+                  break;
+                }
+
+                hash[s[start]] = false;
+                start++;
             }
+        }
+        ans = Math.max(ans, i - start + 1);
+    }
+    return ans;
+};

98888ef494f90d0700ee6dc11f7b96e24f20a258
yang123guo <289656703@qq.com>
2017-06-16 23:23:14 +0800

    haode

--- leetCode/3 Longest Substring Without Repeating Characters.js
+++ leetCode/3 Longest Substring Without Repeating Characters.js
@@ -1,3 +1,8 @@
 // Source : https://leetcode.com/problems/longest-substring-without-repeating-characters/
 // Author : Han Zichi
 // Date   : 2016-08-05
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
